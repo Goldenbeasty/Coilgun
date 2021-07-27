17bea08675d461479615fdf481f7252f3e811bdd
Goldenbeasty <73758693+Goldenbeasty@users.noreply.github.com>
2021-07-27 14:01:18 +0300

    Added Voltmeter functionality

--- src/interact.cpp
+++ src/interact.cpp
@@ -2,11 +2,19 @@
 
 const int safety = 4;
 const int ohterarduino = 5;
+const int volt = A7;
+
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 100000.0; // resistance of R1 (100K) -see text!
+float R2 = 10000.0; // resistance of R2 (10K) - see text!
+int value = 0;
 
 void setup(){
 attachInterrupt(digitalPinToInterrupt(2),trigger,RISING);
 attachInterrupt(digitalPinToInterrupt(2),releasetrigger,FALLING);
 pinMode(safety, INPUT);
+pinMode(volt, INPUT);
 }
 
 void trigger(){
@@ -20,6 +28,12 @@ void releasetrigger(){
 
 void loop(){
     // God's Blessing on this Wonderful World!
-
+   // read the value at analog input
+   value = analogRead(volt);
+   vout = (value * 5.0) / 1024.0; // see text
+   vin = vout / (R2/(R1+R2)); 
+   if (vin<0.09) {
+   vin=0.0;//statement to quash undesired reading !
+   }
     // TODO OLED control, Voltage monitoring
 }

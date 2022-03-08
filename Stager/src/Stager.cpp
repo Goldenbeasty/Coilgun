516c7d51da12cf25f97e0e22dccf2d42c16272e0
Goldenbeasty <goldenbeaster@gmail.com>
2022-03-08 21:41:50 +0200

    fixed for else problems, reduced to 3 coils

--- Stager/src/Stager.cpp
+++ Stager/src/Stager.cpp
@@ -26,32 +26,32 @@ void resetcoil(); // needs to be declared when using .cpp files in VS code, if y
 bool allowcoil1 = true;
 bool allowcoil2 = true;
 bool allowcoil3 = true;
-bool allowcoil4 = true;
-bool allowcoil5 = true;
-bool allowcoil6 = true;
+// bool allowcoil4 = true;
+// bool allowcoil5 = true;
+// bool allowcoil6 = true;
 
 // To make sure that a used coil will not turn back on
 volatile bool coilhasbeenused1 = true;
 volatile bool coilhasbeenused2 = true;
 volatile bool coilhasbeenused3 = true;
-volatile bool coilhasbeenused4 = true;
-volatile bool coilhasbeenused5 = true;
-volatile bool coilhasbeenused6 = true; 
+// volatile bool coilhasbeenused4 = true;
+// volatile bool coilhasbeenused5 = true;
+// volatile bool coilhasbeenused6 = true; 
 
 void setup() {// just the setup to assign pinmodes // probably can be done more easly, but I don't care, just keep it simple
   pinMode(sensorpin1, INPUT);
   pinMode(sensorpin2, INPUT);
   pinMode(sensorpin3, INPUT);
-  pinMode(sensorpin4, INPUT);
-  pinMode(sensorpin5, INPUT);
-  pinMode(sensorpin6, INPUT);
+  // pinMode(sensorpin4, INPUT);
+  // pinMode(sensorpin5, INPUT);
+  // pinMode(sensorpin6, INPUT);
 
   pinMode(mosfetpin1, OUTPUT);
   pinMode(mosfetpin2, OUTPUT);
   pinMode(mosfetpin3, OUTPUT);
-  pinMode(mosfetpin4, OUTPUT);
-  pinMode(mosfetpin5, OUTPUT);
-  pinMode(mosfetpin6, OUTPUT);
+  // pinMode(mosfetpin4, OUTPUT);
+  // pinMode(mosfetpin5, OUTPUT);
+  // pinMode(mosfetpin6, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(2),resetcoil,RISING); // Interrupt from the other microcontroller
 }
@@ -60,97 +60,85 @@ void resetcoil(){ // Enables coils for fireing
   coilhasbeenused1 = false;
   coilhasbeenused2 = false;
   coilhasbeenused3 = false;
-  coilhasbeenused4 = false;
-  coilhasbeenused5 = false;
-  coilhasbeenused6 = false;
+  // coilhasbeenused4 = false;
+  // coilhasbeenused5 = false;
+  // coilhasbeenused6 = false;
 }
 
 void loop(){ // Currently takes around 60 microseconds to loop, probably can be optimized, if you want you can write it in assembely language if you wish so :)
-  if (coilhasbeenused1 == false){ // Checks if the coil is allowed to work, if the sensor is clear, if the bullet has already passed
-    if (allowcoil1){
-      if (digitalRead(sensorpin1) == LOW){
-        digitalWrite(mosfetpin1, HIGH);
-      }
-    }
+  if (coilhasbeenused1 == false and allowcoil1 and digitalRead(sensorpin1) == LOW){ // Checks if the coil is allowed to work, if the sensor is clear, if the bullet has already passed
+    digitalWrite(mosfetpin1, HIGH);
   }
   else{
     digitalWrite(mosfetpin1, LOW);
     coilhasbeenused1 = true;
   }
   
-  if (coilhasbeenused2 == false){ // Same as coil 1
-    if (allowcoil2){
-      if (digitalRead(sensorpin2) == LOW){
-        digitalWrite(mosfetpin2, HIGH);
-      }
-    }
+  if (coilhasbeenused2 == false and allowcoil2 and digitalRead(sensorpin2) == LOW and coilhasbeenused1){
+    digitalWrite(mosfetpin2, HIGH);
   }
   else{
     digitalWrite(mosfetpin2, LOW);
-    coilhasbeenused2 = true;
+    if (coilhasbeenused1){
+        coilhasbeenused2 = true;
+    }
   }
 
-  if (coilhasbeenused3 == false){ // Checks if the coil is allowed to work, if the sensor is clear, if the bullet has passed it's coil and if the bullet has passed two coils prior
-    if (coilhasbeenused1){
-      if (allowcoil3){
-        if (digitalRead(sensorpin3) == LOW){
-          digitalWrite(mosfetpin3,HIGH);
-        }
-      }
-    }
+  if (coilhasbeenused3 == false and coilhasbeenused1 and allowcoil3 and digitalRead(sensorpin3) == LOW){ // Checks if the coil is allowed to work, if the sensor is clear, if the bullet has passed it's coil and if the bullet has passed two coils prior
+    digitalWrite(mosfetpin3,HIGH);
   } 
   else{
     digitalWrite(mosfetpin3, LOW);
-    if (coilhasbeenused1){
+    if (coilhasbeenused2){
       coilhasbeenused3 = true;
     }
   }
   
-  if (coilhasbeenused4 == false){ // Same as coil 3
-    if (coilhasbeenused2){
-      if (allowcoil4){
-        if (digitalRead(sensorpin4) == LOW){
-          digitalWrite(mosfetpin4, HIGH);
-        }
-      }
-    }
-  }
-  else{
-    digitalWrite(mosfetpin4, LOW);
-    if (coilhasbeenused2){
-      coilhasbeenused4 = true;
-    }
-  }
+  // if (coilhasbeenused4 == false){ // Same as coil 3
+  //   if (coilhasbeenused2){
+  //     if (allowcoil4){
+  //       if (digitalRead(sensorpin4) == LOW){
+  //         digitalWrite(mosfetpin4, HIGH);
+  //       }
+  //     }
+  //   }
+  // }
+  // else{
+  //   digitalWrite(mosfetpin4, LOW);
+  //   if (coilhasbeenused2){
+  //     coilhasbeenused4 = true;
+  //   }
+  // }
 
-  if (coilhasbeenused5 == false){ // Same as coil 3
-    if (coilhasbeenused3){
-      if (allowcoil5){
-        if (digitalRead(sensorpin5) == LOW){
-          digitalWrite(mosfetpin5, HIGH);
-        }
-      }
-    }
-  }
-  else{
-    digitalWrite(mosfetpin5,LOW);
-    if (coilhasbeenused3 == false){
-      coilhasbeenused5 = true;
-    }
-  }
+  // if (coilhasbeenused5 == false){ // Same as coil 3
+  //   if (coilhasbeenused3){
+  //     if (allowcoil5){
+  //       if (digitalRead(sensorpin5) == LOW){
+  //         digitalWrite(mosfetpin5, HIGH);
+  //       }
+  //     }
+  //   }
+  // }
+  // else{
+  //   digitalWrite(mosfetpin5,LOW);
+  //   if (coilhasbeenused3 == false){
+  //     coilhasbeenused5 = true;
+  //   }
+  // }
 
-  if (coilhasbeenused6 == false){ // Same as coil 3
-    if (coilhasbeenused4){
-      if (allowcoil6){
-        if (digitalRead(sensorpin6) == LOW){
-          digitalWrite(mosfetpin6,HIGH);
-        }
-      }
-    }
-  }
-  else{
-    digitalWrite(mosfetpin6, LOW);
-    if (coilhasbeenused4){
-      coilhasbeenused6 = true;
-    }
-  }
+  // if (coilhasbeenused6 == false){ // Same as coil 3
+  //   if (coilhasbeenused4){
+  //     if (allowcoil6){
+  //       if (digitalRead(sensorpin6) == LOW){
+  //         digitalWrite(mosfetpin6,HIGH);
+  //       }
+  //     }
+  //   }
+  // }
+  // else{
+  //   digitalWrite(mosfetpin6, LOW);
+  //   if (coilhasbeenused4){
+  //     coilhasbeenused6 = true;
+  //   }
+  // }
 }
